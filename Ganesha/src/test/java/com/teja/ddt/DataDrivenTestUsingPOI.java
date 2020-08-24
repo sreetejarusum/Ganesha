82bfca237375e04ff39adbbb8c7f2126377cbafd
sreetejarusum <sreetejarusum@gmailcom>
2020-08-25 01:43:50 +0530

    Adding Comments

--- Ganesha/src/test/java/com/teja/ddt/DataDrivenTestUsingPOI.java
+++ Ganesha/src/test/java/com/teja/ddt/DataDrivenTestUsingPOI.java
@@ -13,58 +13,64 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DataDrivenTestUsingPOI {
+
+	public String file = "C:\\Teja\\Teja2.0\\FrameWork\\Ganesha\\data\\FinalPlan.xlsx";
+	public FileInputStream fis;
+	public Properties props;
+	public XSSFWorkbook workbook;
+	public int sheetCount;
+	public String sheetName = "td2";
+	public String columnName = "TC's";
+	public int columnNumber;
+	public String data = "Checkout";
+	public ArrayList<String> testdata;
+
+	/**
+	 * This Utility Accpts SheetName,ColumnName and Data
+	 * @param SheetName
+	 * @param columnName
+	 * @param data
+	 * @return
+	 * @throws IOException
+	 */
 	
-	public  String file="C:\\Teja\\Teja2.0\\FrameWork\\Ganesha\\data\\FinalPlan.xlsx";
-	public  FileInputStream fis;
-	public  Properties props;
-	public  XSSFWorkbook workbook;
-	public  int sheetCount;
-	public  String sheetName="td2";
-	public  String columnName="TC's";
-	public  int columnNumber;
-	public  String data="Checkout";
-	public  ArrayList<String> testdata;
-	
-	
-	public ArrayList<String> getTestData(String SheetName,String columnName,String data) throws IOException{
-		testdata=new ArrayList<String>();
-		fis=new FileInputStream(file);
-		workbook=new XSSFWorkbook(fis);
-		
-		XSSFSheet sheet=workbook.getSheet(sheetName);
-		
-		Iterator<Row> rows=sheet.iterator();
-		Row firstRow=rows.next();
-		
-		Iterator<Cell> cells=firstRow.cellIterator();
-		int i=0;
-		while(cells.hasNext()) {
-			
-			if(cells.next().getStringCellValue().equalsIgnoreCase(columnName)) {
-				
-				columnNumber=i;
+	public ArrayList<String> getTestData(String SheetName, String columnName, String data) throws IOException {
+		testdata = new ArrayList<String>();
+		fis = new FileInputStream(file);
+		workbook = new XSSFWorkbook(fis);
+
+		XSSFSheet sheet = workbook.getSheet(sheetName);
+
+		Iterator<Row> rows = sheet.iterator();
+		Row firstRow = rows.next();
+
+		Iterator<Cell> cells = firstRow.cellIterator();
+		int i = 0;
+		while (cells.hasNext()) {
+
+			if (cells.next().getStringCellValue().equalsIgnoreCase(columnName)) {
+
+				columnNumber = i;
 			}
-		 i++;
+			i++;
 		}
-		
-		while(rows.hasNext()) {
-			Row r=rows.next();
-			if(r.getCell(columnNumber).getStringCellValue().equalsIgnoreCase(data)) {
-				
-				Iterator<Cell> cl=r.cellIterator();
-				while(cl.hasNext()) {
-					
-					Cell c=cl.next();
-					
-					
-					
-					if(c.getCellType()==CellType.STRING) {
+
+		while (rows.hasNext()) {
+			Row r = rows.next();
+			if (r.getCell(columnNumber).getStringCellValue().equalsIgnoreCase(data)) {
+
+				Iterator<Cell> cl = r.cellIterator();
+				while (cl.hasNext()) {
+
+					Cell c = cl.next();
+
+					if (c.getCellType() == CellType.STRING) {
 						System.out.println(c.getStringCellValue());
-					}else {
+					} else {
 						System.out.println(NumberToTextConverter.toText(c.getNumericCellValue()));
 
 					}
-					
+
 				}
 			}
 		}
