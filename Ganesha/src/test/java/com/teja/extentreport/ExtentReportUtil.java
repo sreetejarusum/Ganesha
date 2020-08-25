7af36e5057f0513ab12a18322f7d9de2ce04d360
sreetejarusum <sreetejarusum@gmailcom>
2020-08-25 20:57:11 +0530

    Adding Extent Reporter.

--- Ganesha/src/test/java/com/teja/extentreport/ExtentReportUtil.java
+++ Ganesha/src/test/java/com/teja/extentreport/ExtentReportUtil.java
@@ -1,55 +1,59 @@
 package com.teja.extentreport;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReportUtil {
-	
+
 	public ExtentReports extent;
 
-
-
 	@BeforeTest
-	public void config(){
+	public void config() {
 
-	// ExtentReports , ExtentSparkReporter
+		// ExtentReports , ExtentSparkReporter
 
-	String path =System.getProperty("user.dir")+"\\reports\\index.html";
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
 
-	ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 
-	reporter.config().setReportName("Web Automation Results");
+		reporter.config().setReportName("TIME Result");
 
-	reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setDocumentTitle("Test Results");
 
-	extent =new ExtentReports();
+		
+		
+		extent = new ExtentReports();
 
-	extent.attachReporter(reporter);
-
-	extent.setSystemInfo("Tester", "Rahul Shetty");
+		extent.attachReporter(reporter);
+		
+		extent.setSystemInfo("Tester", "TIME");
 
 	}
 
 	@Test
 
-	public void initialDemo()
+	public void initialDemo() {
 
-	{
+		ExtentTest test = extent.createTest("Initial Demo");
 
-	ExtentTest test= extent.createTest("Initial Demo");
+		System.setProperty("webdriver.chrome.driver", "C:\\Teja\\push\\Selenium\\N\\cd\\chromedriver.exe");
 
-	System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
-	WebDriver driver =new ChromeDriver();
+		driver.get("https://ndtv.com");
 
-	driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
 
-	System.out.println(driver.getTitle());
+		driver.close();
 
-	driver.close();
+		test.fail("Result do not match");
 
-	//test.fail("Result do not match");
-
-	extent.flush();
+		extent.flush();
 
 	}
 
